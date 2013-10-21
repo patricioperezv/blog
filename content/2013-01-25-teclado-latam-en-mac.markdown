9e44fe7c9c3cdad1f18353e1269b7a89f5aadfdf
Patricio Pérez <patricio.perez@ceinf.cl>
2013-10-21 00:24:32 -0300

    Commit inicial, me gusta como va

---
+++ content/2013-01-25-teclado-latam-en-mac.markdown
@@ -0,0 +1,19 @@
+layout: post
+title: "Teclado latam en mac"
+date: 2013-01-25 20:27
+comments: true
+categories: Mac
+
+Realmente no me acostumbro al teclado del mac, quizás es porque el resto del tiempo uso teclados latinos y este sea de españa, la verdad no lo se.    
+Lo que si se es que me cabree, pesque [Ukelele](http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=ukelele "Ukelele") y vandalice un poco el layout español
+
+![Ukele latam pato :D](http://f.cl.ly/items/063R0C39272m042p3v0V/Captura%20de%20pantalla%202013-01-25%20a%20la%28s%29%2020.37.11.png)
+
+Los cambios realizados (si no me falla la memoria):
+
+* Shift + 3 inserta un gato
+* Las teclas de las llaves({}) al ser presionadas una vez insertan la llave(Sin apretar alt + llave)
+* Shift + tecla de llave inserta los corchetes cuadrados
+
+Y ahora picar codigo es más agradable :D    
+[Descargar layout teclado latino](http://cl.ly/0r1O2e3s3K30)
---
+++ pelicanconf.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Patricio Pérez'
+SITENAME = u"pato's corner"
+SITEURL = ''
+SITETAGLINE = 'Geeky bastard y algo busquilla'
+SITEDESCR = 'Cosas que considero entretes, a veces me equivoco (?)'
+
+TIMEZONE = 'America/Santiago'
+
+DEFAULT_LANG = u'es-CL'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Seba', 'http://alumnos.informatica.utem.cl/~srocha/'),
+          ('Python.org', 'http://python.org/'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'lannisport'
+TYPOGRIFY = True
+PATH = 'content'
+
+STATIC_PATHS = ['images']
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# Settings personales
+GOOGLE_ANALYTICS_CODE = 'UA-44982980-1'
+GITHUB_URL = 'https://github.com/pperez/'
+TWITTER_URL = 'http://twitter.com/janitux'
+FACEBOOK_URL = 'http://facebook.com/janitux'
+CONTACT_URL = 'about.html'
+SITELOGO = 'images/me.png'
+
+# Plugins
+PLUGIN_PATH = 'plugins'
+PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.include_code', 'pelican_gist'
+           ]
